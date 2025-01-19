0d9c2b6cc343de75e0d5cfcb0ca1216a7e7031b1
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-19 14:53:36 +0530

    made the changes

--- project/all.sh
+++ project/all.sh
@@ -6,3 +6,14 @@ fi
 cat /opt/nginx.conf | envsubst >/etc/nginx/nginx.conf
 
 nginx -g 'daemon off;'
+
+if [ -z "${VITE_API_URL}" ]; then
+  echo "Input VITE_API_URL is missing"
+  exit 1
+fi
+
+# Perform environment variable substitution and create the final nginx.conf
+cat /opt/nginx.conf | envsubst > /etc/nginx/nginx.conf
+
+# Start Nginx
+nginx -g 'daemon off;'
