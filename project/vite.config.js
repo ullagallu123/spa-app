52724e6f9739390a15166973ea07d79eebbe8d7c
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-01-19 15:46:58 +0530

    made the chaneges

--- project/vite.config.js
+++ project/vite.config.js
@@ -4,7 +4,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: process.env.VITE_API_URL,  // Fallback if not set
+        target: process.env.VITE_API_URL,
         changeOrigin: true,
         secure: false,
       },
