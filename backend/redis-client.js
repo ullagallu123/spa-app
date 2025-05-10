8f025e5e86f04cfff6d6348730822240a872dbdc
SIVARAMAKRISHNA <51481385+ullagallu123@users.noreply.github.com>
2025-05-10 12:44:01 +0530

    update the code

--- backend/redis-client.js
+++ backend/redis-client.js
@@ -21,9 +21,9 @@ const redis = new Redis({
   host: process.env.REDIS_HOST || 'localhost', // Use the Elasticache Redis endpoint here
   port: process.env.REDIS_PORT || 6379, // Ensure this is 6379 or the correct port
   password: process.env.REDIS_PASSWORD || undefined,
-  tls: {
-    rejectUnauthorized: false // This disables SSL certificate verification (can be enabled for more security)
-  }
+  // tls: {
+  //   rejectUnauthorized: false // This disables SSL certificate verification (can be enabled for more security)
+  // }
 });
 
 redis.on('connect', () => {
